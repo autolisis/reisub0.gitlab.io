4b9c889c35f6351bf6d370736b8114a277dadfc1
reisub0 <reisub0@gmail.com>
2019-02-09 18:08:08 +0530

    Edits pelicanconf to include robots
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,12 +14,24 @@ STATIC_PATHS = [
 ]
 EXTRA_PATH_METADATA = {
     # 'extra/custom.css': {'path': 'custom.css'},
-    # 'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/logo.jpg': {'path': 'logo.jpg'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/CNAME': {'path': 'CNAME'},
-    'extra/LICENSE': {'path': 'LICENSE'},
-    'extra/README': {'path': 'README'},
+    'extra/robots.txt': {
+        'path': 'robots.txt'
+    },
+    'extra/logo.jpg': {
+        'path': 'logo.jpg'
+    },
+    'extra/favicon.ico': {
+        'path': 'favicon.ico'
+    },
+    'extra/CNAME': {
+        'path': 'CNAME'
+    },
+    'extra/LICENSE': {
+        'path': 'LICENSE'
+    },
+    'extra/README': {
+        'path': 'README'
+    },
 }
 
 THEME = './theme/pelican-svbhack'
@@ -29,7 +41,6 @@ TIMEZONE = 'Asia/Kolkata'
 
 DEFAULT_LANG = 'en'
 
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -38,8 +49,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Social widget
-SOCIAL = (('Github', 'https://github.com/reisub0/'),
-          )
+SOCIAL = (('Github', 'https://github.com/reisub0/'), )
 
 DEFAULT_PAGINATION = 5
 
